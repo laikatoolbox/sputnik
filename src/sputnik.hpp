2d821c05a68ddad98f4434e76ebe8baf37d4457b
laikatoolbox <laikatoolbox@outlook.com>
2026-02-07 23:53:31 -0700

    more work

--- src/sputnik.hpp
+++ src/sputnik.hpp
@@ -14,10 +14,11 @@ namespace Sputnik {
     class Sector {
     public:
         Sector();
+        Sector(const Sector &sector);
         ~Sector();
 
-        std::map<std::string, std::map<std::string, std::string>> sections = {};
-        std::multimap<std::string, std::map<std::string, std::string>> objects = {};
+        std::map<std::string, std::map<std::string, std::string>> sections;
+        std::multimap<std::string, std::map<std::string, std::string>> objects;
         
         std::string value(std::string key);
         std::vector<std::string> valueAsArray(std::string key);
@@ -36,6 +37,8 @@ namespace Sputnik {
     class File {
     private:
         void clear();
+        std::map<std::string, std::string> *rootOfRoot();
+        
     public:
         File();
         ~File();
@@ -83,7 +86,12 @@ std::vector<std::string_view> Sputnik::splitString(std::string_view text, char d
 
 // SECTOR
 Sputnik::Sector::Sector() {
+    this->sections.insert({"root", {}});
+}
 
+Sputnik::Sector::Sector(const Sector &sector) {
+    this->sections = sector.sections;
+    this->objects = sector.objects;
 }
 
 Sputnik::Sector::~Sector() {
@@ -102,9 +110,13 @@ Sputnik::File::~File() {
 void Sputnik::File::clear() {
     // Clear the sectors
     this->sectors.clear();
-    // Add in root sector
-    Sputnik::Sector *rootSector = new Sputnik::Sector();
-    this->sectors.insert({"root", *rootSector});
+    //this->sectors.insert({"root", Sputnik::Sector()});
+
+}
+
+std::map<std::string, std::string> *Sputnik::File::rootOfRoot()
+{
+    return &(sectors["root"].sections["root"]);
 }
 
 Sputnik::ParseStatus Sputnik::File::parseText(std::string text) {
@@ -124,39 +136,62 @@ Sputnik::ParseStatus Sputnik::File::parseFile(std::string fileName) {
         std::string line;
         // Variables to keep track of state that spans multiple lines
         Sputnik::LineState lineState = Sputnik::LineState::AtRoot;
-        std::map<std::string, std::string> *currentSectionOrObject = nullptr;
+        std::map<std::string, std::string> *currentSectionOrObject = this->rootOfRoot();
 
         while (std::getline(file, line)) {
             //std::cout << line << std::endl;
             bool sectionStart = line.starts_with(':');
             bool objectStart = line.starts_with('@');
             if (sectionStart || objectStart) { // sections and objects share a lot of logic
+                bool kickBackToRoot = false;
                 std::string sectionOrObjectName(line.begin()+1, line.end());
+                
+                // An empty section or object definition means we're being 
+                // kicked back to root
+                if (sectionOrObjectName.length() == 0) {
+                    kickBackToRoot = true;
+                    sectionOrObjectName = "root";
+                }
 
-                // Check if a sector is specified in the name. If not, the sector defaults to root.
+                // Get the sector based on the section or object's definition
+                // If it has a '>', the text to the left is the sector's name.
+                // If it doesn't, the sector is the default "root" sector.
                 std::vector<std::string_view> sectorSplit = Sputnik::splitString(sectionOrObjectName, '>');
                 std::string sectorName = "root";
                 if (sectorSplit.size() > 1) {
                     sectorName = std::string(sectorSplit[0]);
                 }
+                Sputnik::Sector *sector = &(this->sectors[sectorName]);
 
-                // Get the sector
-                Sputnik::Sector sector = this->sectors[sectorName];
-
-                if (sectionStart) {
+                // Logic that differs between sections and objects
+                if (sectionStart) { // section specific logic
                     lineState = Sputnik::LineState::InSection;
-                    currentSectionOrObject = &sector.sections[sectionOrObjectName]; 
-                } else {
+                    sector->sections.insert({sectionOrObjectName, {}});
+                    currentSectionOrObject = &(sector->sections[sectionOrObjectName]); 
+                } else { // object specific logic
                     lineState = Sputnik::LineState::InObject;
-                    // replace this with creating and inserting a new object
-                    currentSectionOrObject = &sector.objects[sectionOrObjectName]; 
+                    // create a new object and append it
+                    std::map<std::string, std::string> object = {};
+                    sector->objects.emplace(sectionOrObjectName, object);
+                    currentSectionOrObject = &object; 
+                }
+
+                if (kickBackToRoot) {
+                    lineState = Sputnik::LineState::AtRoot;
+                    currentSectionOrObject = this->rootOfRoot();
                 }
 
                 std::cout << sectionOrObjectName << std::endl;
             } else if (line.starts_with(';')) { // comment
                 // do nothing
             } else if (line.find("=") != std::string::npos) { // key=value line
-                // todo: get key and value
+                // get the key and value
+                std::vector<std::string_view> kvSplit = Sputnik::splitString(line, '=');
+                std::string key = std::string(kvSplit[0]);
+                std::string value = std::string(kvSplit[1]);
+
+                // assign it in the current object
+                currentSectionOrObject->emplace(key, value);
             }
         }
 
