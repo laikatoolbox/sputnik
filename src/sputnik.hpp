b8ecf82b12e7381098a51016543f19966f311827
laikatoolbox <laikatoolbox@outlook.com>
2026-02-21 11:44:26 -0700

    Add assertions

--- src/sputnik.hpp
+++ src/sputnik.hpp
@@ -2,16 +2,20 @@
 #include <map>
 #include <vector>
 #include <fstream>
+#include <algorithm>
+#include <ranges>
 
 #ifndef SPUTNIK_H
 #define SPUTNIK_H
 
-namespace Sputnik {
+namespace Sputnik
+{
     /// @brief A set of values and objects related to each other.
     /// think of it like a "namespace" in C++, which I was originally
     /// going to call it but decided on "sector" to avoid conflicts
     /// with the reserved keyword "namespace".
-    class Sector {
+    class Sector
+    {
     public:
         Sector();
         Sector(const Sector &sector);
@@ -19,12 +23,12 @@ namespace Sputnik {
 
         std::map<std::string, std::map<std::string, std::string>> sections;
         std::multimap<std::string, std::map<std::string, std::string>> objects;
-        
-        std::string value(std::string key);
-        std::vector<std::string> valueAsArray(std::string key);
+
+        std::map<std::string, std::string> &section(std::string sectionName);
     };
 
-    struct ParseStatus {
+    struct ParseStatus
+    {
         // flag if the parse was a success
         bool success = true;
         // if not a success, not the line and error message
@@ -34,11 +38,12 @@ namespace Sputnik {
         std::string errorMessage = "";
     };
 
-    class File {
+    class File
+    {
     private:
         void clear();
         std::map<std::string, std::string> *rootOfRoot();
-        
+
     public:
         File();
         ~File();
@@ -48,70 +53,113 @@ namespace Sputnik {
         ParseStatus parseText(std::string text);
         ParseStatus parseFile(std::string fileName);
 
-        Sector root();
-        Sector sector(std::string key);
+        Sputnik::Sector root();
+        Sputnik::Sector &sector(std::string key);
+
+        std::string value(std::string key, std::string section, std::string sector);
+        std::vector<std::string_view> valueAsArray(std::string key, std::string section, std::string sector);
     };
 
-    enum class LineState {
+    enum class LineState
+    {
         AtRoot,
         InSection,
         InObject
     };
 
     std::vector<std::string_view> splitString(std::string_view text, char delimeter);
+    void replaceAll(std::string& subject, const std::string& search, const std::string& replace);
+    void sanitize(std::string& text);
+    void desanitize(std::string& text);
 }
 
 // String ops
-std::vector<std::string_view> Sputnik::splitString(std::string_view text, char delimeter)
-{
-    std::vector<std::string_view> result; 
-    auto left = text.begin(); 
+std::vector<std::string_view> Sputnik::splitString(std::string_view text, char delimeter) {
+    std::vector<std::string_view> result;
+    auto left = text.begin();
 
-    for (auto it = left; it != text.end(); ++it) 
-    { 
-        if (*it == delimeter) 
-        { 
-            result.emplace_back(&*left, it - left); 
-            left = it + 1; 
-        } 
-    } 
-    
-    if (left != text.end()) {
-        result.emplace_back(&*left, text.end() - left); 
+    for (auto it = left; it != text.end(); ++it) {
+        if (*it == delimeter) {
+            result.emplace_back(&*left, it - left);
+            left = it + 1;
+        }
     }
 
-    return result; 
+    if (left != text.end()) {
+        result.emplace_back(&*left, text.end() - left);
+    }
+
+    return result;
+}
+
+void Sputnik::replaceAll(std::string& subject, const std::string& search, const std::string& replace) {
+    size_t pos = 0;
+    while ((pos = subject.find(search, pos)) != std::string::npos) {
+         subject.replace(pos, search.length(), replace);
+         pos += replace.length();
+    }
 }
 
 
+void Sputnik::sanitize(std::string& text) {
+    Sputnik::replaceAll(text, "$", "$dl");
+    Sputnik::replaceAll(text, ":", "$cl");
+    Sputnik::replaceAll(text, "=", "$eq");
+    Sputnik::replaceAll(text, ";", "$sc");
+    Sputnik::replaceAll(text, "\r", "$r");
+    Sputnik::replaceAll(text, "\n", "$n");
+    Sputnik::replaceAll(text, "@", "$at");
+    Sputnik::replaceAll(text, ">", "$gt");
+    Sputnik::replaceAll(text, "<", "$lt");
+}
+
+void Sputnik::desanitize(std::string& text) {    
+    Sputnik::replaceAll(text, "$cl", ":");
+    Sputnik::replaceAll(text, "$eq", "=");
+    Sputnik::replaceAll(text, "$sc", ";");
+    Sputnik::replaceAll(text, "$r", "\r");
+    Sputnik::replaceAll(text, "$n", "\n");
+    Sputnik::replaceAll(text, "$at", "@");
+    Sputnik::replaceAll(text, "$gt", ">");
+    Sputnik::replaceAll(text, "$lt", "<");
+    Sputnik::replaceAll(text, "$dl", "$");
+}
+
 // SECTOR
-Sputnik::Sector::Sector() {
+Sputnik::Sector::Sector()
+{
     this->sections.insert({"root", {}});
 }
 
-Sputnik::Sector::Sector(const Sector &sector) {
+Sputnik::Sector::Sector(const Sector &sector)
+{
     this->sections = sector.sections;
     this->objects = sector.objects;
 }
 
-Sputnik::Sector::~Sector() {
+Sputnik::Sector::~Sector()
+{
+}
 
+std::map<std::string, std::string> &Sputnik::Sector::section(std::string sectionName)
+{
+    return this->sections[sectionName];
 }
 
 // FILE
-Sputnik::File::File() {
-
+Sputnik::File::File()
+{
 }
 
-Sputnik::File::~File() {
-
+Sputnik::File::~File()
+{
 }
 
-void Sputnik::File::clear() {
+void Sputnik::File::clear()
+{
     // Clear the sectors
     this->sectors.clear();
-    //this->sectors.insert({"root", Sputnik::Sector()});
-
+    // this->sectors.insert({"root", Sputnik::Sector()});
 }
 
 std::map<std::string, std::string> *Sputnik::File::rootOfRoot()
@@ -119,36 +167,42 @@ std::map<std::string, std::string> *Sputnik::File::rootOfRoot()
     return &(sectors["root"].sections["root"]);
 }
 
-Sputnik::ParseStatus Sputnik::File::parseText(std::string text) {
+Sputnik::ParseStatus Sputnik::File::parseText(std::string text)
+{
     Sputnik::ParseStatus status;
     return status;
 }
 
-Sputnik::ParseStatus Sputnik::File::parseFile(std::string fileName) {
+Sputnik::ParseStatus Sputnik::File::parseFile(std::string fileName)
+{
     Sputnik::ParseStatus status;
     std::ifstream file(fileName);
 
-    if (file.is_open()) {
+    if (file.is_open())
+    {
         // File opened, so clear (reset all values) this Sputnik file
         this->clear();
-        
+
         // Loop by line
         std::string line;
         // Variables to keep track of state that spans multiple lines
         Sputnik::LineState lineState = Sputnik::LineState::AtRoot;
         std::map<std::string, std::string> *currentSectionOrObject = this->rootOfRoot();
 
-        while (std::getline(file, line)) {
-            //std::cout << line << std::endl;
+        while (std::getline(file, line))
+        {
+            // std::cout << line << std::endl;
             bool sectionStart = line.starts_with(':');
             bool objectStart = line.starts_with('@');
-            if (sectionStart || objectStart) { // sections and objects share a lot of logic
+            if (sectionStart || objectStart)
+            { // sections and objects share a lot of logic
                 bool kickBackToRoot = false;
-                std::string sectionOrObjectName(line.begin()+1, line.end());
-                
-                // An empty section or object definition means we're being 
+                std::string sectionOrObjectName(line.begin() + 1, line.end());
+
+                // An empty section or object definition means we're being
                 // kicked back to root
-                if (sectionOrObjectName.length() == 0) {
+                if (sectionOrObjectName.length() == 0)
+                {
                     kickBackToRoot = true;
                     sectionOrObjectName = "root";
                 }
@@ -158,48 +212,81 @@ Sputnik::ParseStatus Sputnik::File::parseFile(std::string fileName) {
                 // If it doesn't, the sector is the default "root" sector.
                 std::vector<std::string_view> sectorSplit = Sputnik::splitString(sectionOrObjectName, '>');
                 std::string sectorName = "root";
-                if (sectorSplit.size() > 1) {
+                if (sectorSplit.size() > 1)
+                {
                     sectorName = std::string(sectorSplit[0]);
+                    sectionOrObjectName = std::string(sectorSplit[1]);
                 }
                 Sputnik::Sector *sector = &(this->sectors[sectorName]);
 
                 // Logic that differs between sections and objects
-                if (sectionStart) { // section specific logic
+                if (sectionStart)
+                { // section specific logic
                     lineState = Sputnik::LineState::InSection;
-                    sector->sections.insert({sectionOrObjectName, {}});
-                    currentSectionOrObject = &(sector->sections[sectionOrObjectName]); 
-                } else { // object specific logic
+
+                    // Only create a new section if the section doesn't already exist
+                    if (!sector->sections.contains(sectionOrObjectName))
+                    {
+                        std::map<std::string, std::string> object = {};
+                        sector->sections.emplace(sectionOrObjectName, object);
+                    }
+
+                    currentSectionOrObject = &(sector->sections[sectionOrObjectName]);
+                }
+                else
+                { // object specific logic
                     lineState = Sputnik::LineState::InObject;
                     // create a new object and append it
                     std::map<std::string, std::string> object = {};
                     sector->objects.emplace(sectionOrObjectName, object);
-                    currentSectionOrObject = &object; 
+                    currentSectionOrObject = &object;
                 }
 
-                if (kickBackToRoot) {
+                if (kickBackToRoot)
+                {
                     lineState = Sputnik::LineState::AtRoot;
                     currentSectionOrObject = this->rootOfRoot();
                 }
 
                 std::cout << sectionOrObjectName << std::endl;
-            } else if (line.starts_with(';')) { // comment
+            }
+            else if (line.starts_with(';'))
+            { // comment
                 // do nothing
-            } else if (line.find("=") != std::string::npos) { // key=value line
+            }
+            else if (line.find("=") != std::string::npos)
+            { // key=value line
                 // get the key and value
                 std::vector<std::string_view> kvSplit = Sputnik::splitString(line, '=');
                 std::string key = std::string(kvSplit[0]);
                 std::string value = std::string(kvSplit[1]);
 
-                // assign it in the current object
-                currentSectionOrObject->emplace(key, value);
+                // assign it
+                (*currentSectionOrObject)[key] = value;
             }
         }
 
         file.close();
     }
 
-
     return status;
 }
 
-#endif 
+Sputnik::Sector &Sputnik::File::sector(std::string key)
+{
+    return this->sectors[key];
+}
+
+std::string Sputnik::File::value(std::string key, std::string section = "root", std::string sector = "root")
+{
+    Sputnik::Sector &foundSector = this->sector(sector);
+    std::map<std::string, std::string> &foundSection = foundSector.section(section);
+    return foundSection[key];
+}
+
+std::vector<std::string_view> Sputnik::File::valueAsArray(std::string key, std::string section = "root", std::string sector = "root")
+{
+    std::string value = this->value(sector, key);
+    return Sputnik::splitString(value, ';');
+}
+#endif
--- src/sputnik_test.cpp
+++ src/sputnik_test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <assert.h>
 #include "sputnik.hpp"
 
 
@@ -14,24 +15,67 @@ void printSplit(std::string title, std::string text, char delimeter) {
     std::cout << "=== End of Split ===" << std::endl;
 }
 
+void testSanitize() {
+    // test sanitizing
+    std::string text = ":, =, ;, \r, \n, @, >, <, $";
+    Sputnik::sanitize(text);
+    assert(text == "$cl, $eq, $sc, $r, $n, $at, $gt, $lt, $dl");
+    
+    // test desanitizing
+    Sputnik::desanitize(text);
+    assert(text == ":, =, ;, \r, \n, @, >, <, $");
+    
+    std::cout << "Sanitize/Desanitize test passed" << std::endl;
+} 
+
 void testStringSplit() {
     printSplit("Semicolon", "a;b;c", ';');
     printSplit("End line", "a\nb\nc\n\r\n\r", '\n');
+    
+    std::cout << "String split test passed" << std::endl;
 }
 
-void testSputnikFileRead() {
+void testKitchenSink() {
     std::cout << "Test reading a file named \"KitchenSink.spk\"" << std::endl;
 
-    Sputnik::File *sputnikFile = new Sputnik::File();
-    Sputnik::ParseStatus fileStatus = sputnikFile->parseFile("samples/KitchenSink.spk");
+    Sputnik::File *spkFile = new Sputnik::File();
+    Sputnik::ParseStatus spkStatus = spkFile->parseFile("samples/KitchenSink.spk");
 
+    // File parse successfully
+    assert(spkStatus.success);
 
-    std::cout << fileStatus.success << std::endl;
+    // We should only have 2 sectors:
+    // the default "root" sector and a sector called "root 2"
+    assert(spkFile->sectors.size() == 2);
+    assert(spkFile->sectors.contains("root"));
+    assert(spkFile->sectors.contains("sector 2"));
+
+    // Test getting a value from a defined sector and section
+    std::string sector2FavoriteColor = spkFile->value("color", "favorites", "sector 2");
+    assert(sector2FavoriteColor == "blue");
+
+    // Test getting a value the default sector root
+    // The intial value is green, but a later value sets it to red, overwriting it
+    std::string rootFavoriteColor = spkFile->value("color", "favorites");
+    assert(rootFavoriteColor == "red");
+
+    // Test that the section key=values are merging correctly
+    // Both animal and food should be populated 
+    std::string rootFavoriteAnimal = spkFile->value("animal", "favorites");
+    std::string rootFavoriteFood = spkFile->value("food", "favorites");
+    assert(rootFavoriteAnimal == "cat");
+    assert(rootFavoriteFood == "pizza");
+
+    // sanity test: uncomment this and it should fail
+    //assert(rootFavoriteFood == "hotdog");
+    
+    std::cout << "Kitchen sink test passed" << std::endl;
 }
 
 int main() {
     testStringSplit();
-    testSputnikFileRead();
+    testSanitize();
+    testKitchenSink();
 
     return 0;
 }
